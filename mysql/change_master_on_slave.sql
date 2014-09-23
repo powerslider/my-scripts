7a831238b95787ed5c6005d273061749f903c112
Tsvetan Dimitrov <tsvetan.dimitrov23@gmail.com>
2014-09-23 13:21:28 +0300

    A sql query for attaching a master to a slave in a mysql replication.

---
+++ mysql/change_master_on_slave.sql
@@ -0,0 +1,8 @@
+change master to
+  master_host='example.host.com',
+  master_user='replication',
+  master_password='pass',
+  master_port=3306,
+  master_log_file='mysql-bin.000001',
+  master_log_pos=28289,
+  master_connect_retry=10;
